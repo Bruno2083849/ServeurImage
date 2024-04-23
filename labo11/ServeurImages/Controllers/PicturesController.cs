f309548194e10bc153982d2b827e59349e2492dc
Bruno Rukundo <2083849@cegepmontpetit.ca>
2024-04-23 18:13:08 -0400

    init

---
+++ labo11/ServeurImages/Controllers/PicturesController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServeurImages.Data;
+using ServeurImages.Models;
+
+namespace ServeurImages.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        private readonly ServeurImagesContext _context;
+
+        public PicturesController(ServeurImagesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pictures
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Picture>>> GetPicture()
+        {
+          if (_context.Picture == null)
+          {
+              return NotFound();
+          }
+            return await _context.Picture.ToListAsync();
+        }
+
+        // GET: api/Pictures/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Picture>> GetPicture(int id)
+        {
+          if (_context.Picture == null)
+          {
+              return NotFound();
+          }
+            var picture = await _context.Picture.FindAsync(id);
+
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            return picture;
+        }
+
+        
+        // POST: api/Pictures
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Picture>> PostPicture(Picture picture)
+        {
+          if (_context.Picture == null)
+          {
+              return Problem("Entity set 'ServeurImagesContext.Picture'  is null.");
+          }
+            _context.Picture.Add(picture);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPicture", new { id = picture.Id }, picture);
+        }
+
+        // DELETE: api/Pictures/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePicture(int id)
+        {
+            if (_context.Picture == null)
+            {
+                return NotFound();
+            }
+            var picture = await _context.Picture.FindAsync(id);
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            _context.Picture.Remove(picture);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+      
+    }
+}
---
+++ labo11/ServeurImages/Models/Picture.cs
@@ -0,0 +1,11 @@
+﻿namespace ServeurImages.Models
+{
+    public class Picture
+    {
+        // Étape 1: Ajouter les champs nécessaires.
+
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string mimeType { get; set; }
+    }
+}
